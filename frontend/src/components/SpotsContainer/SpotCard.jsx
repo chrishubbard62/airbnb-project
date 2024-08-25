6d1ef2d19dd9eaf3220da08930d6739e6436f089
chris <chris.hubbard.dev@gmail.com>
2024-08-25 16:52:41 -0700

    fixed a bit of styling for the nav bar

--- frontend/src/components/SpotsContainer/SpotCard.jsx
+++ frontend/src/components/SpotsContainer/SpotCard.jsx
@@ -5,14 +5,13 @@ import { FaStar } from "react-icons/fa";
 function SpotCard({spot}) {
   return (
     <Link to={`/${spot.id}`} className="spot-card">
-
       <img className='spot-card-image' src={spot.previewImage} alt={`${spot.description}`}/>
       <span className='tool-tip'>{spot.name}</span>
       <div className='state-stars'>
         <div>{spot.city}, {spot.state}</div>
         <div>{spot.avgRating !== 'NEW!' && <FaStar className='star'/>}{spot.avgRating}</div>
       </div>
-      <div className='price'>${spot.price.toFixed(2)}</div>
+      <div className='price'>${spot.price.toFixed(2)}/Night</div>
     </Link>
   )
 
