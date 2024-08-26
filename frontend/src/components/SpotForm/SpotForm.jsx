9199641c5f579802927336d497b841430af29e76
chris <chris.hubbard.dev@gmail.com>
2024-08-26 14:31:10 -0700

    finished form validations ready for the interaction with backend

--- frontend/src/components/SpotForm/SpotForm.jsx
+++ frontend/src/components/SpotForm/SpotForm.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react"
+import './SpotForm.css'
+
+const ENDINGS = ['.png', '.jpg', '.jpeg']
 
 
 function SpotForm() {
@@ -16,65 +19,85 @@ function SpotForm() {
   const [valErrors, setValErrors] = useState({})
   const [createToggle, setCreateToggle] = useState(true)
 
+
   useEffect(() => {
     const errors = {}
-    if(country.length < 1 ) errors.country = 'Country is required'
-    if(address.length < 1)  errors.address = 'Address is required'
-    if(city.length < 1) errors.city = 'City is required'
-    if(state.length < 1) errors.state = 'State is required'
+    if (country.length < 1) errors.country = 'Country is required'
+    if (address.length < 1) errors.address = 'Address is required'
+    if (city.length < 1) errors.city = 'City is required'
+    if (state.length < 1) errors.state = 'State is required'
+    if (lat < -90 || lat > 90) errors.lat = 'Latitude muse be between -90 and 90'
+    if (lng < -180 || lng > 180) errors.lng = 'Longitude must be between -180 and 180'
+    if (description.length < 30) errors.description = 'description needs to be a minimum of 30 characters'
+    if (name.length < 1) errors.name = 'Name is required'
+    if (price < .01) errors.price = 'Price must be greater than $0'
+
+    if (!ENDINGS.some((ending) => prevImage.endsWith(ending))) errors.prevImage = 'Image URL must end with .png .jpg, or .jpeg'
+    if (prevImage.length < 1) errors.prevImage = 'Preview image is required'
+    if (!Object.values(errors).length) setCreateToggle(false)
     setValErrors(errors)
+  }, [country, address, city, state, description, name, price, prevImage, lat, lng])
 
-
-  },[country, address, city, state, description, name, price, prevImage, lat, lng])
-
-
-  return <form className="new-spot-form" style={{display: 'flex', flexDirection: 'column', width: 500}}>
+  return <form className="new-spot-form" style={{ display: 'flex', flexDirection: 'column', width: 500 }}>
     <h2>Create a new Spot</h2>
-    <p>Where's your place located? Guests will only get your exact address once they booked a reservation.</p>
+    <p>Where&apos;s your place located? Guests will only get your exact address once they booked a reservation.</p>
     <div>
-      <label htmlFor="country">Country</label>{valErrors.country && <span> {valErrors.country}</span>}
+      <label htmlFor="country">Country</label>
+      {valErrors.country && <span className="errors"> {valErrors.country}</span>}
     </div>
     <input
       value={country}
       onChange={(e) => setCountry(e.target.value)}
       placeholder="Country"
       type="text"
-      name="country"/>
-    <label htmlFor="address">Street Address</label>
+      name="country" />
+    <div>
+      <label htmlFor="address">Street Address</label>
+      {valErrors.address && <span className="errors"> {valErrors.address}</span>}
+    </div>
     <input
       value={address}
       onChange={(e) => setAddress(e.target.value)}
       placeholder="Address"
       type="text"
-      name="address"/>
-    <div className="city-state-container">
-      <div><label htmlFor="city">City</label></div>
-      <input
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        placeholder="City"
-        type="text"
-        name="city"/>
-      <div><label htmlFor="state">State</label></div>
-      <input
-        value ={state}
-        onChange={(e) => setState(e.target.value)}
-        placeholder="State"
-        type="text"
-        name="state"/>
+      name="address" />
+    <div>
+      <label htmlFor="city">City</label>
+      {valErrors.city && <span className="errors"> {valErrors.city}</span>}
     </div>
-    <div className="lat-lng-container">
-      <label htmlFor="latitude">Latitude</label>
+    <input
+      value={city}
+      onChange={(e) => setCity(e.target.value)}
+      placeholder="City"
+      type="text"
+      name="city" />
+    <div>
+      <label htmlFor="state">State</label>
+      {valErrors.state && <span className="errors"> {valErrors.state}</span>}
+    </div>
+    <input
+      value={state}
+      onChange={(e) => setState(e.target.value)}
+      placeholder="State"
+      type="text"
+      name="state" />
+      <div>
+        <label htmlFor="latitude">Latitude</label>
+        {valErrors.lat && <span className="errors"> {valErrors.lat}</span>}
+      </div>
       <input
         value={lat}
         onChange={(e) => setLat(e.target.value)}
         placeholder="Latitude"
         type="number"
-        name= "latitude"
+        name="latitude"
         min={-90.0}
         max={90.0}
-        />
-      <label htmlFor="longitude">Longitude</label>
+      />
+      <div>
+        <label htmlFor="longitude">Longitude</label>
+        {valErrors.lng && <span className="errors"> {valErrors.lng}</span>}
+      </div>
       <input
         value={lng}
         onChange={(e) => setLng(e.target.value)}
@@ -82,11 +105,11 @@ function SpotForm() {
         name="longitude"
         min={-180.0}
         max={180.0}
-        />
-    </div>
+      />
+
     <h3>Describe your place to guests</h3>
     <p>Mention the best features of your space, any special amentities like
-    fast wifi or parking, and what you love about the neighborhood.</p>
+      fast wifi or parking, and what you love about the neighborhood.</p>
     <textarea
       value={description}
       onChange={(e) => setDescription(e.target.value)}
@@ -94,28 +117,30 @@ function SpotForm() {
       name="description"
       id="">
     </textarea>
+    {valErrors.description ? <div className="errors">{valErrors.description}</div> : <div className="hide">Validated</div>}
     <h3>Create a title for your spot</h3>
-    <p>Catch guests' attention with a spot title that highlights what makes
-    your place special.</p>
+    <p>Catch guests&apos; attention with a spot title that highlights what makes
+      your place special.</p>
     <input
       value={name}
       onChange={(e) => setName(e.target.value)}
       placeholder="Name of your spot"
       type="text"
-      name="name"/>
-    <hr />
+      name="name" />
+    {valErrors.name ? <div className="errors">{valErrors.name}</div> : <div className="hide">Validated</div>}
     <h3>Set a base price for your spot</h3>
     <p>Competitive pricing can help your listing stand out and rank higher
-    in search results.</p>
+      in search results.</p>
     <label>$
-    <input
-      value={price}
-      onChange={(e) => setPrice(e.target.value)}
-      type="number"
-      name="price"
-      placeholder="Price per night (USD)" />
+      <input
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+        min={.01}
+        type="number"
+        name="price"
+        placeholder="Price per night (USD)" />
     </label>
-    <hr />
+    {valErrors.price ? <div className="errors">{valErrors.price}</div> : <div className="hide">Validated</div>}
     <h3>Liven up your spot with photos</h3>
     <p>Submit a link to at least one photo to publish your spot</p>
     <input
@@ -123,12 +148,18 @@ function SpotForm() {
       onChange={(e) => setPrevImage(e.target.value)}
       type="text"
       name='preview'
-      placeholder="Preview Image URL"/>
-    <input type="text" name='image' placeholder="Feature Coming Soon" disabled={true}/>
-    <input type="text" name='image' placeholder="Feature Coming Soon" disabled={true}/>
-    <input type="text" name='image'placeholder="Feature Coming Soon" disabled={true}/>
+      placeholder="Preview Image URL" />
+    {valErrors.prevImage ? <div className="errors">{valErrors.prevImage}</div> : <div className="hide">Validated</div>}
+    <input type="text" name='image' placeholder="Feature Coming Soon" disabled={true} />
+    {<div className='hide'>Validated</div>}
+    <input type="text" name='image' placeholder="Feature Coming Soon" disabled={true} />
+    {<div className='hide'>Validated</div>}
+    <input type="text" name='image' placeholder="Feature Coming Soon" disabled={true} />
+    {<div className='hide'>Validated</div>}
     <hr />
-    <button>Create Spot</button>
+    <button
+      disabled={createToggle}
+    >Create Spot</button>
 
 
   </form>
