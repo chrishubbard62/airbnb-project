c5491eadd99119ee00ef4502ad19fc6b4bc99414
chris <chris.hubbard.dev@gmail.com>
2024-10-23 09:32:27 -0700

    worked it out! dates are grayed out

--- backend/db/seeders/20240803155319-demo-booking.js
+++ backend/db/seeders/20240803155319-demo-booking.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { addHours } = require('date-fns')
+
 const {Booking} = require('../models')
 
 let options = {};
@@ -13,32 +15,32 @@ module.exports = {
   {
     spotId: 1,
     userId: 3,
-    startDate: new Date('2024-10-01'),
-    endDate: new Date('2024-10-05')
+    startDate: addHours(new Date('2024-10-01'), 14),
+    endDate: addHours(new Date('2024-10-05'), 14)
   },
   {
     spotId: 2,
     userId: 1,
-    startDate: new Date('2024-10-10'),
-    endDate: new Date('2024-10-15')
+    startDate: addHours(new Date('2024-11-20'), 14),
+    endDate: addHours(new Date('2024-11-26'), 14)
   },
   {
     spotId: 3,
     userId: 2,
-    startDate: new Date('2024-11-01'),
-    endDate: new Date('2024-11-05')
+    startDate: addHours(new Date('2024-11-15'), 14),
+    endDate: addHours(new Date('2024-11-19'), 14)
   },
   {
     spotId: 2,
     userId: 1,
-    startDate: new Date('1998-11-11'),
-    endDate: new Date('1998-11-16')
+    startDate: addHours(new Date('2024-11-11'), 14),
+    endDate: addHours(new Date('2024-11-16'), 14)
   },
   {
     spotId: 2,
     userId: 1,
-    startDate: new Date('2025-10-10'),
-    endDate: new Date('2025-10-15')
+    startDate: addHours(new Date('2025-11-02'), 14),
+    endDate: addHours(new Date('2025-11-07'), 14)
   },
    ], { validate: true })
   },
